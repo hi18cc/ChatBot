67c40b14716d272695bf44f2b119233257e71439
owen-bryan <odog654@gmail.com>
2022-04-22 16:43:11 -0400

    Update ChatBotTests.py

--- Chatbot_Application/Chatbot/ChatBotTests.py
+++ Chatbot_Application/Chatbot/ChatBotTests.py
@@ -1,7 +1,21 @@
 import unittest
-import request
 import json
+from bot import chat
 
-
-class TestLinl(unittest.TestCase):
+class TestAskingForTickets(unittest.TestCase):
 	
+	def testTicketsNormal(self):
+		response = chat ("Where can I buy tickets");
+		assert response == "Tickets and ticket information can be obtained here https://niagara2022games.ca/tickets/", "Does not return the link"
+		
+	def testTicketsPhraseMisspelled(self):
+		response = chat ("Whe ca I bye tickets");
+		assert response == "Tickets and ticket information can be obtained here https://niagara2022games.ca/tickets/", "Does not return the link due to misspelling everything in the phrase"
+		
+	def testTicketsMisspelled(self):
+		response = chat ("Whe ca I bye tick");
+		assert response == "Tickets and ticket information can be obtained here https://niagara2022games.ca/tickets/", "Does not return the link due to misspelling of tickets"
+		
+		
+if __name__ == "__main__":
+	unittest.main()
