aa30eb1b878fc16e2e645f70e19145550621441a
Ridwaan <ridwaantoure@gmail.com>
2022-04-22 19:08:46 -0400

    Updated databse  and made sure update medals working.

--- Chatbot_Application/Chatbot/chatbot_backend/seleniumScraper/views.py
+++ Chatbot_Application/Chatbot/chatbot_backend/seleniumScraper/views.py
@@ -42,7 +42,7 @@ URL = 'https://cg2022.gems.pro/Result/Calendar.aspx?SetLanguage=en-CA&GameDay_GU
 
     
 def view_name(request):
-    fill_player_data()
+    update_medals()
     return HttpResponse("Done")
 
 
@@ -268,9 +268,10 @@ def update_medals():
     """       
 
     url = "https://cg2019.gems.pro/Result/MedalList.aspx?SetLanguage=en-CA"
-    driver.get(url)
+    
     driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=chrome_options)
     driver.implicitly_wait(2)
+    driver.get(url) 
     
 
     conn = SQLMethods.create_connection(database) # connects to our database
