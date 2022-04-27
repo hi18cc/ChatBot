ac19ea6d09897f79be6febdd3104b6645367a1d0
owen-bryan <odog654@gmail.com>
2022-04-27 18:07:14 -0400

    Revert "Merge pull request #28 from hi18cc/Nimish-template"
    This reverts commit d819845c2d11fc5b28cd3c4628800af887a052ff, reversing
    changes made to 814f3ec1b17928e179dfc3a1b37f47aeff9626bb.

--- Chatbot_Application/Chatbot/templates/chat.js
+++ Chatbot_Application/Chatbot/templates/chat.js
@@ -24,7 +24,10 @@ class chat {
         document.getElementById ("msg").value = "";
     }
 
-    
+    help()
+    {
+        window.open("index.html", "_self");
+    }
     
     history() {
         let html = "Niagara Games 2022 Chatbot Conversation History \n\n";
