439808647d073db747f253591835b3ecf5abba99
Ridwaan <ridwaantoure@gmail.com>
2022-04-26 21:42:05 -0400

    Chatbot has initial suggestions when first loading up

--- Chatbot_Application/Chatbot/templates/chat.js
+++ Chatbot_Application/Chatbot/templates/chat.js
@@ -19,6 +19,7 @@ class chat {
         node.addEventListener("keyup", ({key}) => {
             if (key === "Enter") this.send(output)
         })
+        this.sendSuggestion(output);
     }
     clear() {
         document.getElementById ("msg").value = "";
@@ -61,15 +62,24 @@ class chat {
         var historyFile = new Blob([html], {type: "text/plain;charset=utf-8"});
         saveAs(historyFile, "NiagaraGames_Chatbot.txt");
     }
+    
     back() {
         window.open("index.html", "_self");
     }
+
+    /**
+    * This method takes the input in the textbox and sends it out to the predict method in the app backend.
+    * Predict will process the input and return a response.
+    * 
+    * @param {Element} out: An element which should contain another element of the class input.
+    */
     send(out) {
-        let content = out.querySelector('input').value;
+        
+        let content = out.querySelector('input').value; // out is the document/page and we get the text from the input element.
         if (content === "") return;
-        let msg1 = { name: "User", message: content };
-        this.messages.push(msg1);
-        this.update(out);
+        let msg1 = { name: "User", message: content }; 
+        this.messages.push(msg1); // Takes the string from input and adds it to the messages array.
+        this.update(out); // Takes the 
 
         fetch('http://127.0.0.1:5000/predict', {
             method: 'POST',
@@ -92,12 +102,47 @@ class chat {
         });
 
     }
+
+    /**
+     * This sends out suggestion questions to the user so they can pick them without having to guess what to ask the bot.
+     * @param {Element} out: The element that is the main class.
+     */
+    sendSuggestion(out){
+        let suggestion = "Welcome to the Chatbot, ask me questions and I'll try to answer them, here are some suggestion questions;"
+        let suggestion1 = { name: "Bot", message: suggestion }; 
+        let suggestion2 = { name: "Quick", message: "How many medals does Ontario have?"}; 
+        let suggestion3 = { name: "Quick", message: "What sport does Spencer Allen play?"};
+        let suggestion4 = { name: "Quick", message: "Where can I buy tickets?"};
+        this.messages.push(suggestion1, suggestion2, suggestion3, suggestion4)
+        this.update(out)
+    }
+    /**
+     * This will take  a string and insert it into the text field so it can be sent out.
+     * @param {Element} 
+     */
+    inputTextInTextBox(out, text){
+        out.querySelector('input').value = text
+    }
+
+    inputAndSendText(out, text){
+        this.inputTextInTextBox(out, text);
+        this.send(out);
+    }
+
+    /**
+    * This method takes an element and outputs a new message to the textbox.
+    * @param {Element} out: An element which should contain another element of the class input.
+    */
     update(out) {
+        
         let html = '';
         this.messages.slice().reverse().forEach(function(item, index) {
             if (item.name === "Bot") {
                 const options = { defaultProtocol: 'https', target: "_blank" };
                 html += '<div class="messages visitor">' + linkifyHtml(item.message, options) + '</div>'
+            } else if (item.name === "Quick"){
+                const options = { defaultProtocol: 'https', target: "_blank" };
+                html += '<div class="messages quick-reply">' + linkifyHtml(item.message, options) + '</div>';
             }
             
             else {
@@ -112,7 +157,16 @@ class chat {
             }
         });
         
+        
         out.querySelector('.output').innerHTML = html;
+        let replies = document.querySelectorAll('.quick-reply')
+
+        replies.forEach((reply) => {
+            reply.addEventListener('click', () =>{
+                this.inputAndSendText(out, reply.textContent);
+            });
+        });
+
         this.scrollFix(out.querySelector('.output'));
     }
 
