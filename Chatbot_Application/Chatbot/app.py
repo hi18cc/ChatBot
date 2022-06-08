6b9e88b97d27ffa7dc8e6bc5e711c7bc3f1ba2fc
Ridwaan <ridwaantoure@gmail.com>
2022-06-08 01:42:31 -0400

    Just set the app to be very vanilla to test

--- Chatbot_Application/Chatbot/app.py
+++ Chatbot_Application/Chatbot/app.py
@@ -1,40 +1,50 @@
 from flask import Flask, request, render_template, request, jsonify
-from flask_cors import CORS
+# from flask_cors import CORS
 from Chatbot_Application.Chatbot.bot import chat
 from flask_apscheduler import APScheduler
 from Chatbot_Application.Chatbot.autoScraper import views
 
 app = Flask(__name__)
 # scheduler = APScheduler()
-CORS(app)
+# CORS(app)
 
 @app.route('/')
-def home():
-    print('Request for index page received')
-    return render_template('html/index.html')
-    # return "Hello, Flask!"
+def index():
+    # print('Request for index page received')
+    # return render_template('html/home.html')
+    return "Hello, Flask!"
 
-@app.post("/predict")
-def predict():
-    text = request.get_json().get("message")
-    response = chat(text)
-    message = {"answer": response}
-    return jsonify(message)
+@app.route('/textbox.html')
+def chatpage():
+    # print('Request for textbox page received')
+    # return render_template('html/textbox.html')
+    return "Hello, Flask!"
 
-def scheduledTask():
-    views.update_games()
 
-def scheduledTask2():
-    views.update_players()
 
-def scheduledTask3():
-    views.update_medals()
+# @app.post("/predict")
+# def predict():
+#     text = request.get_json().get("message")
+#     response = chat(text)
+#     message = {"answer": response}
+#     return jsonify(message)
 
-# if __name__ == '__main__':
+
+
+# def scheduledTask():
+#     views.update_games()
+
+# def scheduledTask2():
+#     views.update_players()
+
+# def scheduledTask3():
+#     views.update_medals()
+
+if __name__ == '__main__':
     # runs task every hour
     # scheduler.add_job(id ='Scheduled task', func = scheduledTask, trigger = 'interval', seconds = 3600)
     # scheduler.add_job(id ='Scheduled task3', func = scheduledTask3, trigger = 'interval', seconds = 3600)
     # runs task every week
     # scheduler.add_job(id ='Scheduled task2', func = scheduledTask2, trigger = 'interval', seconds = 604800)
     # scheduler.start()
-    # app.run()
+    app.run()
